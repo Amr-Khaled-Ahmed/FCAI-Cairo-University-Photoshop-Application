2a2b760c9043f28d088c242c989d789e4f386f3b
mohamedahmed2005 <162487393+mohamedahmed2005@users.noreply.github.com>
2024-03-22 17:16:21 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -21,10 +21,22 @@ void invert_image(string& image_name, Image& primary) {
     }
     primary.saveImage("invert_image.jpg");
 }
+void rotate_image(string& image_name, Image& primary) {
+    Image rotated_image(primary.height, primary.width);
+    for (int i = 0; i < primary.width; i++) {
+        for (int j = 0; j < primary.height; j++) {
+            rotated_image(j, primary.width - 1 - i, 0) = primary(i, j, 0);
+            rotated_image(j, primary.width - 1 - i, 1) = primary(i, j, 1);
+            rotated_image(j, primary.width - 1 - i, 2) = primary(i, j, 2);
+        }
+    }
+    rotated_image.saveImage("rotate_image.jpg");
+}
 int main() {
     cout<<"Welcome to image filters"<<endl<<endl;
     cout<<"1. Grayscale"<<endl;
     cout<<"3. Invert image"<<endl;
+    cout<<"6. Rotate image"<<endl;
     cout<<"7. Exit"<<endl;
     cout<<"Please enter your filter choice: "<<endl;
     string filter_choice;
@@ -32,6 +44,7 @@ int main() {
     while(true){
         if(filter_choice=="1")break;
         else if(filter_choice=="3")break;
+        else if(filter_choice=="6")break;
         else if(filter_choice=="7"){
             cout<<"Thanks for using our image filter application"<<endl;
             exit(0);
@@ -61,14 +74,11 @@ int main() {
 
         }
          if (filter_choice == "1") {
-                 for (int i = 0; i < primary.width; i++) {
-        for (int j = 0; j < primary.height; j++) {
-            for (int k = 0; k < primary.channels; k++) {
-            }
-        }
-                 }
             primary.saveImage("Grayscale.jpg");
         } else if (filter_choice == "3") {
             invert_image(image_name, primary);
+        }
+        else if (filter_choice == "6") {
+           rotate_image(image_name,primary);
         }
 }
