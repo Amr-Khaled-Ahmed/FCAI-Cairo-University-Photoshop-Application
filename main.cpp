7f5990d6c24ee7bf3ca758ee524f96c0f631cdbe
mohamedahmed2005 <162487393+mohamedahmed2005@users.noreply.github.com>
2024-03-22 14:16:17 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,15 +11,27 @@
 #include <regex>
 #include "Image_Class.h"
 using namespace std;
+void invert_image(string& image_name, Image& primary) {
+    for (int i = 0; i < primary.width; i++) {
+        for (int j = 0; j < primary.height; j++) {
+            for (int k = 0; k < primary.channels; k++) {
+                primary(i, j, k) = 255 - primary(i, j, k);
+            }
+        }
+    }
+    primary.saveImage("invert_image.jpg");
+}
 int main() {
     cout<<"Welcome to image filters"<<endl<<endl;
     cout<<"1. Grayscale"<<endl;
+    cout<<"3. Invert image"<<endl;
     cout<<"7. Exit"<<endl;
     cout<<"Please enter your filter choice: "<<endl;
     string filter_choice;
     cin>>filter_choice;
     while(true){
         if(filter_choice=="1")break;
+        else if(filter_choice=="3")break;
         else if(filter_choice=="7"){
             cout<<"Thanks for using our image filter application"<<endl;
             exit(0);
@@ -48,22 +60,10 @@ int main() {
 
 
         }
-        if(filter_choice=="1"){
-            for(int i=0; i<primary.width; i++){
-                for(int j=0; j<primary.height; j++){
-                    for(int k=0; k<primary.channels; k++){
-
-                    }
-                }
-            }
+         if (filter_choice == "1") {
+            // Grayscale filter logic here...
             primary.saveImage("Grayscale.jpg");
+        } else if (filter_choice == "3") {
+            invert_image(image_name, primary);  // Correct way to call the invert_image function
         }
-
-    }
-
-
-
-
-
-
-
+}
